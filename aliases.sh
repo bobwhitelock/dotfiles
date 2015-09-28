7d204ec41a48a9902662c93eea174f2b783b7432
Bob Whitelock <bob.whitelock1@gmail.com>
2015-09-28 10:03:19 +0100

    Add Vacs ssh alias

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,7 @@
 alias pyserver="python -m SimpleHTTPServer 8000"
 
 alias pvm='sshpass -p alces ssh portalvm'
+alias vacsvm='ssh bob@127.0.0.1 -p 9322'
 
 # For 'detached screen'; launches screen for command with same name as command.
 #complete -A command ds TODO: commented for moment as errors in zsh
