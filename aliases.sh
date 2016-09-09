6059b7fd57c6909498582d0d240ae68796c32400
Bob Whitelock <bob.whitelock1@gmail.com>
2016-09-09 18:26:19 +0100

    Remove extract function
    dtrx (`aptitude install dtrx`) is better!

--- aliases.sh
+++ aliases.sh
@@ -182,29 +182,6 @@ swap() {
     mv $TMPFILE "$2"
 }
 
-# Handy Extract Program.
-# From http://tldp.org/LDP/abs/html/sample-bashrc.html.
-extract() {
-    if [ -f "$1" ] ; then
-        case $1 in
-            *.tar.bz2)   tar xvjf "$1"     ;;
-            *.tar.gz)    tar xvzf "$1"     ;;
-            *.bz2)       bunzip2 "$1"      ;;
-            *.rar)       unrar x "$1"      ;;
-            *.gz)        gunzip "$1"       ;;
-            *.tar)       tar xvf "$1"      ;;
-            *.tbz2)      tar xvjf "$1"     ;;
-            *.tgz)       tar xvzf "$1"     ;;
-            *.zip)       unzip "$1"        ;;
-            *.Z)         uncompress "$1"   ;;
-            *.7z)        7z x "$1"         ;;
-            *)           echo "'$1' cannot be extracted via extract()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file!"
-    fi
-}
-
 yaml_validate() {
     ruby -e "require 'yaml'; YAML.load_file('$1')"
 }
