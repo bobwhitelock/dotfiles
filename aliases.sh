8420358de4b2029a38ea737bb62c78edcfe7b786
Bob Whitelock <bob.whitelock1@gmail.com>
2016-11-02 00:01:20 +0000

    Add function for starting Tmux session
    Shares functionality with `add-window` function, but for starting a new
    session instead.

--- aliases.sh
+++ aliases.sh
@@ -182,13 +182,26 @@ launch() {
 # Add new Tmux window in given dir (defaulting to current dir); appropriately
 # named and with panes for shell and Vim open.
 add-window() {
-    local args window_path window_name
+    _tmux-create new-window "$*"
+}
+alias aw="add-window"
 
+# Same as above, except start a new session for the window.
+new-session() {
+    _tmux-create new-session "$*"
+}
+
+# Utility function to be used by above.
+_tmux-create() {
+    local tmux_command args window_path window_name
+
+    tmux_command="$1"
+    shift
     args="$*"
     window_path=$(realpath "${args:-$(pwd)}")
     window_name=$(basename "$window_path")
 
-    tmux new-window \
+    tmux "$tmux_command" \
         -c "$window_path" \
         -n "$window_name" \
         \; split-window -hd -c "$window_path" \
