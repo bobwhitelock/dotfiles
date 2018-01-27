e3053cc381176bdae571f6ea106ceffb91ba923d
Bob Whitelock <bob.whitelock1@gmail.com>
2018-01-27 02:02:20 +0000

    Add elm-bash-completion package

--- zsh/packages.sh
+++ zsh/packages.sh
@@ -3,4 +3,10 @@ PACKAGES_DIR="$DOTFILES/zsh/packages"
 
 source "$PACKAGES_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
+autoload bashcompinit
+bashcompinit
+for f in "$PACKAGES_DIR"/elm-bash-completion/_*; do
+    source "$f"
+done
+
 unset PACKAGES_DIR
