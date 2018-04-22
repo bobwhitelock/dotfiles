b85e0613d4ff6fc412d777047cfc0fc4479cef4f
Bob Whitelock <bob.whitelock1@gmail.com>
2018-04-22 23:23:22 +0100

    Remove non-aliasing from aliases file to zshrc

--- zsh/lib/main.sh
+++ zsh/lib/main.sh
@@ -45,12 +45,7 @@ xrandr_off() {
     conkywonky
 }
 
-# Let Vim interpret keys which would be interpreted by the terminal otherwise
-# (see http://vim.wikia.com/wiki/Map_Ctrl-S_to_save_current_or_new_files).
-alias vim="stty stop '' -ixoff ; vim"
 alias v='vim'
-ttyctl -f
-
 # Ease transition between shell and vim.
 alias :q="exit"
 alias :qa="tmux kill-window"
