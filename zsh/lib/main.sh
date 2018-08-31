0a92c4245abf48e971f3d4142f577d65ada564c7
Bob Whitelock <bob.whitelock1@gmail.com>
2018-08-31 13:14:02 +0100

    Add function to output most used commands

--- zsh/lib/main.sh
+++ zsh/lib/main.sh
@@ -151,3 +151,18 @@ unwindows() {
 
 alias urlencode='ruby -r cgi -e "puts CGI.escape(ARGV[0])"'
 alias urldecode='ruby -r cgi -e "puts CGI.unescape(ARGV[0])"'
+
+# Output commands from history in order from least to most used. Tweaked from
+# https://dev.to/abhinav/which-is-the-most-used-command-in-your-shell-history-5ca1.
+# Columns: usage rank | times used | percent usage | command.
+most_used() {
+    history 1 | \
+        cat | \
+        awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a; }' | \
+        grep -v "./" | \
+        column -c3 -s " " -t | \
+        sort -n | \
+        tac | \
+        nl | \
+        tac
+}
