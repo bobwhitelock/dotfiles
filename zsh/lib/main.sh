e3583eb78e950bfe96dc545c85b79dfc41af71c7
Bob Whitelock <bob.whitelock1@gmail.com>
2019-01-22 19:20:36 +0000

    Add function to run Unused via Docker

--- zsh/lib/main.sh
+++ zsh/lib/main.sh
@@ -187,3 +187,10 @@ tar_dir() {
 pshow() {
     pgrep "$@" | xargs --no-run-if-empty ps -l
 }
+
+# Run https://github.com/joshuaclayton/unused via Docker, without needing to
+# install locally.
+unused_docker() {
+    sudo systemctl start docker && \
+        sudo docker run --rm -it -v "$(pwd)":/code joshuaclayton/unused unused "$@"
+}
