bobwhitelock/dotfiles

commit 3d08ee35cba00ab86499c6e025532a85fa07cd2a

Stop explicitly naming 'secondary' Tmux session
Explicitly naming this means that can't start more than one session with
the 'secondary' profile, as the names for these would conflict; instead
just let Tmux automatically name these with incrementing numbers.

config/terminator/config
@@ -87,7 +87,7 @@
     color_scheme = custom
     cursor_color = "#ff6600"
     cursor_color_fg = False
-    custom_command = unset TMUX; TERM=xterm-256color tmux new-session -s secondary
+    custom_command = unset TMUX; TERM=xterm-256color tmux new-session
     font = DejaVu Sans Mono 15
     foreground_color = "#ffffff"
     login_shell = True

commit b1d206d599d2c60deecea858870b157c1814cd96

Fix TmuxLaunchTerminal
So tslime commands from Vim will automatically run in the launched
terminal again.

This behaviour was broken by
https://github.com/bobwhitelock/dotfiles/commit/3d08ee35cba00ab86499c6e025532a85fa07cd2a.

This commit fixes this while also supporting the behaviour intended by
that commit - as many secondary terminals, with corresponding Tmux
sessions, as needed can now be launched, and the one launched from each
Vim instance will be set as the session/window/pane where tslime
commands from that Vim instance will run.

config/terminator/config
@@ -87,7 +87,7 @@
     color_scheme = custom
     cursor_color = "#ff6600"
     cursor_color_fg = False
-    custom_command = unset TMUX; TERM=xterm-256color tmux new-session
+    custom_command = "$DOTFILES"/libexec/tmux-secondary
     font = DejaVu Sans Mono 15
     foreground_color = "#ffffff"
     login_shell = True

libexec/tmux-secondary
@@ -1,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Utility script to launch secondary Tmux session; called from secondary
+# profile in `config/terminator/config` and intended to be triggered either by
+# `TmuxLaunchTerminal` command in `vimrc` (will use name from
+# "$SESSION_NAME_FILE"), or `W-S-s` Openbox key binding (will use next default
+# Tmux session name).
+
+SESSION_NAME_FILE='/tmp/tmux_session_name'
+
+main() {
+  local tmux_session_name=''
+  if [ -f "$SESSION_NAME_FILE" ]; then
+    tmux_session_name="$(tr -d '\n' < "$SESSION_NAME_FILE")"
+    rm -f "$SESSION_NAME_FILE"
+  fi
+
+  unset TMUX
+  export TERM=xterm-256color
+
+  if [ -n "$tmux_session_name" ]; then
+    tmux new-session -s "$tmux_session_name"
+  else
+    tmux new-session
+  fi
+}
+
+main "$@"

vimrc
@@ -724,12 +724,17 @@ vmap <leader>tS <Plug>SendSelectionToTmux
 
 command! TmuxReset execute "normal <Plug>SetTmuxVars"
 
-" Launch another terminal on leftmost monitor, running Tmux with the same
-" working directory, and set this to be where tslime commands will run.
+" Launch another terminal on leftmost monitor, running Tmux session with
+" unique name and same working directory, and set this to be where tslime
+" commands will run.
 function! TmuxLaunchTerminal()
+  let l:tmux_session_name = 'secondary-' . trim(system('echo $$'))
+  call system('echo '.l:tmux_session_name.' > /tmp/tmux_session_name')
+
   call system('terminator --maximise --profile secondary --geometry  1x1+0+0 &')
+
   let g:tslime = {}
-  let g:tslime['session'] = 'secondary'
+  let g:tslime['session'] = l:tmux_session_name
   let g:tslime['window'] = 1
   let g:tslime['pane'] = 1
 endfunction