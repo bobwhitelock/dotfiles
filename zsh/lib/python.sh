c80b2351cc3d1d3c187e00e57ce51a0da350438b
Bob Whitelock <bob.whitelock1@gmail.com>
2021-03-17 23:23:09 +0000

    Auto-source any available venv when new shell started

--- zsh/lib/python.sh
+++ zsh/lib/python.sh
@@ -1,5 +1,13 @@
 
+alias activate='source venv/bin/activate'
 alias shell='./manage.py shell_plus'
+
+# If there is a local venv present when a new shell is started (e.g.
+# `add_window` has just been run for a Python project), then activate it.
+if [ -f venv/bin/activate ]; then
+    # shellcheck disable=SC1091
+    activate
+fi
 
 urls() {
     local temp_file
