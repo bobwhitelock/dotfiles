6a9727d0737b6f4cb730f912998e0b2aab0a9ec6
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-21 14:36:28 +0100

    Have `rake` and `rails` run at Git root using shims
    Have these always run at the Git root, using the shims within `bin/` (in
    Rake's case only if this is present, to account for non-Rails projects).
    This is useful so these always run in the same way no matter which
    directory within a repo they are run for, and always account for
    additional behaviour that might be added to these shims.

--- zsh/lib/ruby.sh
+++ zsh/lib/ruby.sh
@@ -2,19 +2,35 @@
 alias b='bundle'
 alias be='bundle exec'
 
-alias rc='bin/rails console'
-alias rdr='bin/rails db:rollback'
-alias rdms='bin/rails db:migrate:status'
-alias rdc='bin/rails dbconsole'
-alias rdR='bin/rails db:reset'
-alias rgm='bin/rails g migration'
-alias routes='bin/rails routes'
+rake() {
+    (groot && maybe_bin_rake "$@")
+}
+
+maybe_bin_rake() {
+    if [ -f bin/rake ]; then
+        bin/rake "$@"
+    else
+        command rake "$@"
+    fi
+}
+
+rails() {
+    (groot && bin/rails "$@")
+}
+
+alias rc='rails console'
+alias rdr='rails db:rollback'
+alias rdms='rails db:migrate:status'
+alias rdc='rails dbconsole'
+alias rdR='rails db:reset'
+alias rgm='rails g migration'
+alias routes='rails routes'
 
 rdm() {
-    if bin/rails -T | grep db:migrate:with_data -q; then
-        bin/rails db:migrate:with_data
+    if rails -T | grep db:migrate:with_data -q; then
+        rails db:migrate:with_data
     else
-        bin/rails db:migrate
+        rails db:migrate
     fi
 }
 
