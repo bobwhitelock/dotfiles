b6ec1c40f1b8ac3866f94a68eceb94881c8ab828
Bob Whitelock <bob.whitelock1@gmail.com>
2022-05-03 13:50:09 +0100

    Tweak `PATH` ordering
    `$HOME/bin` should appear before default stuff in `PATH`, in particular
    `/usr/bin`, so my own scripts take precedence (such as my `gs` over
    Ghostscript's `gs`).

--- zsh/env.sh
+++ zsh/env.sh
@@ -3,7 +3,7 @@ export ZSH_DIR="$DOTFILES/zsh"
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
 
-export PATH="$JAVA_HOME/bin:$HOME/.local/bin:$HOME/.rbenv/bin:/usr/local/go/bin:$PATH:$HOME/bin:/usr/local/bin:$HOME/.rvm/bin:./node_modules/.bin:$HOME/.cargo/bin"
+export PATH="$JAVA_HOME/bin:$HOME/.local/bin:$HOME/.rbenv/bin:/usr/local/go/bin:$HOME/bin:$PATH:/usr/local/bin:$HOME/.rvm/bin:./node_modules/.bin:$HOME/.cargo/bin"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # As suggested to make Yeoman work in
