6a3667060d7a1bc8fb14ed095b9212e71f2da5ae
Bob Whitelock <bob.whitelock1@gmail.com>
2023-11-03 13:33:50 +0000

    Also swap SSH key used when swap GitHub accounts
    So the account used for authenticating to push to/pull from GitHub is
    always the same as that used for the GitHub CLI.

--- zsh/lib/git.sh
+++ zsh/lib/git.sh
@@ -1,16 +1,29 @@
 
 gh_login_personal() {
-    _gh_login "$GITHUB_CLI_TOKEN_PERSONAL"
+    _gh_login id_rsa.bob "$GITHUB_CLI_TOKEN_PERSONAL"
 }
 
 gh_login_rescale() {
-    _gh_login "$GITHUB_CLI_TOKEN_RESCALE"
+    _gh_login id_bob-rescale "$GITHUB_CLI_TOKEN_RESCALE"
 }
 
 _gh_login() {
-    local gh_cli_token
-    gh_cli_token="$1"
+    local gh_key_name gh_cli_token
+    gh_key_name="$1"
+    gh_cli_token="$2"
+
+    # Restart SSH so key added below will be the only valid key for GitHub in
+    # the keychain, otherwise the account that ends up authenticated may be
+    # incorrect.
+    restart_ssh_agent
+
+    # Authenticate correct account in Git and output verification.
+    ssh-add ~/.ssh/"$gh_key_name"
+    ssh -T git@github.com
+
+    # Authenticate correct account in GitHub CLI and output verification.
     echo "$gh_cli_token" | gh auth login --with-token
+    gh auth status
 }
 
 alias g='git'
--- zsh/lib/ssh.sh
+++ zsh/lib/ssh.sh
@@ -7,3 +7,18 @@ ssh_remove_last_key() {
         -f ~/.ssh/known_hosts \
         -R "$(history -n | tail -n 1 | cut -d@ -f2)"
 }
+
+restart_ssh_agent() {
+    pkill ssh-agent
+    start_or_reuse_ssh_agent
+}
+
+start_or_reuse_ssh_agent() {
+    # Start ssh-agent and share this between shells. From
+    # https://unix.stackexchange.com/a/217223/229081.
+    if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+      eval `ssh-agent`
+      ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+    fi
+    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+}
