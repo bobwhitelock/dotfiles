0b8dc9d55e3e05482aed183cb55be3d3e022bbe7
Bob Whitelock <bob.whitelock1@gmail.com>
2025-01-23 15:19:53 +0000

    Add alias to easily install a local Python dependency

--- zsh/lib/python.sh
+++ zsh/lib/python.sh
@@ -4,6 +4,12 @@ alias shell='./manage.py shell_plus'
 alias pyprofile='sudo env PATH="$PATH" py-spy record --subprocesses -o "profile-$(date --iso-8601=seconds).svg"'
 alias poetry_cache_clear="rm -rf ~/.cache/pypoetry/"
 
+# Install a local repo, with fuzz selection, for development/debugging. This
+# works best even in a Poetry environment, as Poetry will often hang
+# indefinitely (or just take a long time, whereas this is fast) when adding a
+# new dependency.
+alias pip_install_repo="\$DOTFILES/libexec/open-repo 'pip install -e'"
+
 # If there is a local venv present when a new shell is started (e.g.
 # `add_window` has just been run for a Python project), then activate it.
 if [ -f venv/bin/activate ]; then
