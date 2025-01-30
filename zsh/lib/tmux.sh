439dda193fcdcbaed73245cc7ac7ecd6dc2585ed
Bob Whitelock <bob.whitelock1@gmail.com>
2025-01-30 19:47:49 +0000

    Change `project` alias to fuzzy select from available repos

--- zsh/lib/tmux.sh
+++ zsh/lib/tmux.sh
@@ -1,11 +1,6 @@
 
 alias mux='tmuxinator'
-# Define this as function rather than alias to avoid usual Tmuxinator
-# auto-completion being used to try to select project path, and have standard
-# Zsh path completion used instead.
-project() {
-    mux start project "$@"
-}
+alias project="\$DOTFILES/libexec/fuzz-repo-and-run 'tmuxinator start project'"
 
 alias attach='tmux switch-client -t'
 alias kill_window='tmux kill-window'
