a4947682cde48e0f433d179262b29c4531a06a77
Bob Whitelock <bob.whitelock1@gmail.com>
2025-10-02 12:31:04 +0100

    Add WIP function from experimenting with Codex a while back

---
+++ zsh/lib/llms.sh
@@ -0,0 +1,15 @@
+
+# XXX BW 2025-10-02:  exporting in here breaks things for other things that
+# need some of these env vars like Aider - use a subshell to avoid polluting
+# env? Or a separate script?
+codex() {
+    # XXX BW 2025-10-02: use `run_in_container.sh` for sandboxing - see
+    # https://github.com/openai/codex?tab=readme-ov-file#platform-sandboxing-details
+    nvm use 22
+    # XXX BW 2025-10-02: This env var appears to need to be set for Azure to
+    # work, but the `OPENAI_API_KEY` will actually be used.
+    export AZURE_OPENAI_API_KEY="placeholder"
+    export OPENAI_API_KEY="$AZURE_API_KEY"
+    export AZURE_BASE_URL="$AZURE_API_BASE/openai"
+    command codex --provider azure --model gpt-4.1 --approval-mode full-auto
+}
