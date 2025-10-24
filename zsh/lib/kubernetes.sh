e8b3283b5173bdcb0524ff2914189eda5b88ff87
Bob Whitelock <bob.whitelock1@gmail.com>
2025-10-24 09:50:06 +0100

    Note alternative approach to Kubernetes context aliases

--- zsh/lib/kubernetes.sh
+++ zsh/lib/kubernetes.sh
@@ -5,6 +5,8 @@ alias kcontexts='k config get-contexts'
 # Create an alias for each Kubernetes context.
 # TODO BW 2025-10-17: Give my other contexts more consistent and concise names
 # so this is actually helpful.
+# TODO BW 2025-10-23: Alternatively could switch to just have a FZF alias to
+# allow selecting a Kubernetes context?
 while IFS= read -r context; do
   alias "$context=k config use-context $context"
 done < <(k config get-contexts -o name)
