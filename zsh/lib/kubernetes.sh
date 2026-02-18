62d0271b9a38beb9841a2283b4a0d50fab66c492
Bob Whitelock <bob.whitelock1@gmail.com>
2026-02-18 17:21:20 +0000

    Disable Kubernetes context handling for now
    This was always clunky as just dumps a bunch of aliases into shell, and
    also causes errors on a fresh machine.

--- zsh/lib/kubernetes.sh
+++ zsh/lib/kubernetes.sh
@@ -2,11 +2,9 @@
 alias k='kubectl'
 alias kcontexts='k config get-contexts'
 
-# Create an alias for each Kubernetes context.
-# TODO BW 2025-10-17: Give my other contexts more consistent and concise names
-# so this is actually helpful.
-# TODO BW 2025-10-23: Alternatively could switch to just have a FZF alias to
-# allow selecting a Kubernetes context?
-while IFS= read -r context; do
-  alias "$context=k config use-context $context"
-done < <(k config get-contexts -o name)
+# TODO BW 2026-02-18: If/when need to do Kubernetes work switching across lots
+# of contexts again, set useful names for contexts and add an FZF alias to help
+# switch between them - previousl did this but it's pretty clunky:
+# while IFS= read -r context; do
+#   alias "$context=k config use-context $context"
+# done < <(k config get-contexts -o name)
